6b8f44f43ef37cb04e98543df584dab905dbfa3c
Amir Hossein <amirhosseinabnous@gmail.com>
2025-10-31 11:59:12 +0330

    added main menu

---
+++ Scense/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var start_button = $StartButton
+@onready var name_box = $LineEdit
+
+func _ready():
+	start_button.pressed.connect(_on_start_pressed)
+
+func _on_start_pressed():
+	var text = name_box.text
+	print("Typed text: ", text)  # just printing for now
+	get_tree().change_scene_to_file("res://Scense/game_on.tscn")
